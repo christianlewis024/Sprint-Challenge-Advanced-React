081f0ad2846a3754e6d44983fca623e5bcbd9867
christianlewis024 <58643162+christianlewis024@users.noreply.github.com>
2020-04-10 10:50:57 -0700

    finished mvp and stretch

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
+import PlayerList from "./components/PlayerList";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("renders App without crashing", () => {
+  render(<App />);
+});
+test("header loads", () => {
+  const { getByText } = render(<App />);
+  getByText(/womens world cup/i);
+});
+
+test("render dark mode", () => {
+  const { getByText } = render(<App />);
+  getByText(/dark mode/i);
 });
